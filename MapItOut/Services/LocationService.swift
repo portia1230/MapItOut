82ceaa3bec909d58da7132685dfeb29884ff25cc
portia1230 <qingfeng1230@gmail.com>
2017-07-12 16:33:25 -0700

    no bugs

--- MapItOut/Services/LocationService.swift
+++ MapItOut/Services/LocationService.swift
@@ -19,7 +19,7 @@ struct LocationService{
         sortedEntries = entries.sorted { (entry1, entry2) -> Bool in
             return (entry1.distance(to: location).magnitude) > (entry2.distance(to: location).magnitude)
         }
-        return sortedEntries
+        return sortedEntries.reversed()
         
     }
     
