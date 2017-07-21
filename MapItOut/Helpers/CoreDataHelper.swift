8953335899914828182b0196e6191816868d7662
portia1230 <qingfeng1230@gmail.com>
2017-07-20 22:03:59 -0700

    started on core data

---
+++ MapItOut/Helpers/CoreDataHelper.swift
@@ -0,0 +1,57 @@
+//
+//  CoreDataHelper.swift
+//  EventTracker
+//
+//  Created by Portia Wang on 7/2/17.
+//  Copyright © 2017 Portia Wang. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataHelper {
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static let persistentContainer = appDelegate.persistentContainer
+    static let managedContext = persistentContainer.viewContext
+    
+    //static methods will go here
+    
+    //new activity
+    static func newItem() -> Item {
+        let item = NSEntityDescription.insertNewObject(forEntityName: "Item", into: managedContext) as! Item
+        return item
+    }
+    
+    //save activity
+    static func saveItem() {
+        do{
+            try managedContext.save()
+        } catch let error as NSError{
+            print("could not save \(error)")
+        }
+    }
+    
+    //delete activity
+    static func deleteActivity (item: Item){
+        managedContext.delete(item)
+        saveItem()
+    }
+    
+    //retreive activity
+    
+    static func retrieveActivities() -> [Item] {
+        let fetchRequest = NSFetchRequest<Item>(entityName: "Item")
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            return results
+        } catch let error as NSError {
+            print("Could not fetch \(error)")
+        }
+        return []
+    }
+}
+
+
+
+
+
