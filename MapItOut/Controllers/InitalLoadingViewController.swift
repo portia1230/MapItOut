d633c0935b00736d6a0c2036579ae2dc631f1f97
portia1230 <qingfeng1230@gmail.com>
2017-07-25 09:44:46 -0700

    progress bar changed to ui progress bar

--- MapItOut/Controllers/InitalLoadingViewController.swift
+++ MapItOut/Controllers/InitalLoadingViewController.swift
@@ -13,26 +13,28 @@ class InitalLoadingViewController: UIViewController {
     
     //MARK: - Properties
     
-    @IBOutlet weak var circularProgress: CustomCircularProgress!
+    @IBOutlet weak var progressView: UIProgressView!
+
     
     //MARK: - Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.progressView.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         self.view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-        circularProgress.startAngle = -90
+        
+        progressView.setProgress(0.0, animated: true)
         //circularProgress.progress = 10
         
         UserService.items(for: User.currentUser, completion: { (entries) in
             var i = 0
             if entries.count != 0{
-                let increaseAngle = Double( 350 / (entries.count) )
+                let increaseAngle = Float( 1 / (entries.count) )
                 while i < entries.count{
                     let imageView = UIImageView()
                     let url = URL(string: entries[i].imageURL)
@@ -51,13 +53,15 @@ class InitalLoadingViewController: UIViewController {
                     item.key = entries[i].key
                     item.image = imageView.image
                     CoreDataHelper.saveItem()
-                    self.circularProgress.animate(toAngle: self.circularProgress.angle + Double(10 + increaseAngle), duration: 1.0, completion: nil)
+                    self.progressView.setProgress(self.progressView.progress + increaseAngle, animated: true)
+                    
                     if i == entries.count - 1{
                         UIView.transition(with: self.view.superview!, duration: 0.25, options: .transitionCrossDissolve, animations: { _ in
                             self.parent?.viewWillAppear(true)
                             self.view.removeFromSuperview()
                         })
                     }
+                    
                     i += 1
 
                 }
